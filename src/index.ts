eb35ba38fd44bb16bddd1455071aa1272f222f37
caique.moraes <caique.moraes@compasso.com.br>
2022-06-14 19:13:17 -0300

    adicionado swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express"
-
+import swaggerUi from 'swagger-ui-express'
 import { usersRoutes } from "./routes/users.routes"
 
 const app = express()
 
 app.use(express.json())
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./swagger.json')))
 app.use("/users", usersRoutes)
 
 export { app }
